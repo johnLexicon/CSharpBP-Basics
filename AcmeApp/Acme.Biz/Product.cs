c755ca9ddd48005355b1b22c696eadf5cf167cc4
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-09 11:41:16 +0100

    Added constructors

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -11,6 +11,19 @@ namespace Acme.Biz
         private string description;
         private int? productId;
 
+        public Product()
+        {
+            Console.WriteLine("Created an instant of the class Product");
+        }
+
+        public Product(int id, string name, string description) : this()
+        {
+            ProductId = id;
+            ProductName = name;
+            Description = description;
+            Console.WriteLine($"Product name: {ProductName}");
+        }
+
         public string ProductName { get => productName; set => productName = value; }
         public string Description { get => description; set => description = value; }
         public int? ProductId { get => productId; set => productId = value; }
