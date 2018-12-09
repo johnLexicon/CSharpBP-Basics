299eecf27b1b1b15a53e012635dcc592f60d2cd6
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-09 17:50:30 +0100

    Changes to fields and properties

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -7,13 +7,17 @@ namespace Acme.Biz
     public class Product
     {
 
+        public readonly decimal MinimumPrice = .5m;
+
         private string productName;
         private string description;
         private int? productId;
+        private Vendor productVendor;
 
         public Product()
         {
             Console.WriteLine("Created an instant of the class Product");
+            MinimumPrice = .99m;
         }
 
         public Product(int id, string name, string description) : this()
@@ -22,11 +26,27 @@ namespace Acme.Biz
             ProductName = name;
             Description = description;
             Console.WriteLine($"Product name: {ProductName}");
+            if (ProductName != null && ProductName.StartsWith("Bulk", StringComparison.Ordinal))
+            {
+                //The readonly field can be assigned multiple times as long as the it is in a constructor.
+                MinimumPrice = 9.99m;
+            }
         }
 
-        public string ProductName { get => productName; set => productName = value; }
+        public string ProductName { get => productName?.Trim(); set => productName = value; }
         public string Description { get => description; set => description = value; }
         public int? ProductId { get => productId; set => productId = value; }
+        public Vendor ProductVendor { 
+            get {
+                //Lazy loading example (instantiate the object when needed)
+                if(productVendor is null)
+                {
+                    productVendor = new Vendor();
+                }
+                return productVendor;
+          } 
+            set => productVendor = value; 
+          }
 
         public string SayHello()
         {
