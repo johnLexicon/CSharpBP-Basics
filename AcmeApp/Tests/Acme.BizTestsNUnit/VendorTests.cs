5bb945558aa101576208932d8ac091a95064c3f6
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-10 04:34:21 +0100

    Added tests for PlaceOrder method.

--- AcmeApp/Tests/Acme.BizTestsNUnit/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTestsNUnit/VendorTests.cs
@@ -51,5 +51,55 @@ namespace Acme.BizTestsBizTestsNUnit
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [Test()]
+        public void PlaceOrder_Test()
+        {
+            //Arrange
+            Vendor vendor = new Vendor();
+            Product product = new Product(1, "Saw", "5 inches jagged saw");
+            bool expected = true;
+
+            //Act
+            bool actual = vendor.PlaceOrder(product, 4);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrder_NullProduct_Test()
+        {
+            //Arrange
+            Vendor vendor = new Vendor();
+            Product product = null;
+
+            //Act
+            var act = vendor.PlaceOrder(product, 4);
+
+            //Assert
+            //Expected exception
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PlaceOrder_QuantityLessThanOne_Test()
+        {
+            //Arrange
+            Vendor vendor = new Vendor();
+            Product product = new Product()
+            {
+                ProductId = 1,
+                ProductName = "Saw",
+                Description = "5 inches jagged saw"
+            };
+
+            //Act
+            var act = vendor.PlaceOrder(product, 0);
+
+            //Assert
+            //Expected exception
+        }
     }
 }
